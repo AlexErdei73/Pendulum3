5d3963ddb473c57ec8fcf76110553dbdd4cc0dbf
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-01-06 20:32:34 +0000

    Add boilerplate to use Canvas for drawing

--- src/Pendulum3.java
+++ src/Pendulum3.java
@@ -1,5 +1,18 @@
-public class Pendulum3 {
+import java.awt.*;
+class Pendulum3 extends Canvas {
+  Pendulum3() {
+    setSize(400, 400);
+    Frame pictureFrame = new Frame("Driven, Damped, Pendulum");
+    Panel canvasPanel = new Panel();
+    canvasPanel.add(this);
+    pictureFrame.add(canvasPanel);
+    pictureFrame.pack();
+    pictureFrame.setVisible(true);
+  }
+  public void paint(Graphics g) {
+    g.drawString("Hello World!", 160, 200);
+  }
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    new Pendulum3();
   }
 }
