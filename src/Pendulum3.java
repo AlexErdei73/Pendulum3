2b28a230ce97a2eb838734b9f5609aa719466a87
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-01-06 22:06:04 +0000

    Draw pendulum rotated by theta angle

--- src/Pendulum3.java
+++ src/Pendulum3.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 class Pendulum3 extends Canvas {
+  double theta = 30.0 / 180.0 * Math.PI;
   Pendulum3() {
     setSize(400, 400);
     Frame pictureFrame = new Frame("Driven, Damped, Pendulum");
@@ -10,7 +11,14 @@ class Pendulum3 extends Canvas {
     pictureFrame.setVisible(true);
   }
   public void paint(Graphics g) {
-    g.drawString("Hello World!", 160, 200);
+    int x = 200 - (int) Math.round(150 * Math.sin(this.theta));
+    int y = 200 + (int) Math.round(150 * Math.cos(this.theta));
+    g.setColor(Color.blue);
+    g.drawOval(195, 195, 10, 10);
+    g.setColor(Color.black);
+    g.drawLine(200, 200, x, y);
+    g.setColor(Color.red);
+    g.drawOval(x - 15, y - 15, 30, 30);
   }
   public static void main(String[] args) {
     new Pendulum3();
